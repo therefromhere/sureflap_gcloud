7bebf2672f56551ec3b5695ec339c0bec3e4af52
John Carter <john@therefromhere.org>
2022-09-10 16:42:32 +1200

    Fix logging

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 import flask
 import sureflap_gcloud
 
@@ -6,6 +8,9 @@ def update(request: flask.Request):
     """
     Google cloud function entrypoint
     """
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
+
     retval = {"status": "ok"}
 
     sureflap_gcloud.set_curfew()
