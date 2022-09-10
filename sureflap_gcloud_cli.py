893433c7a095cba0086f7cd2892fc7437ebdc24f
John Carter <john@therefromhere.org>
2022-09-10 16:11:38 +1200

    Set logging

--- sureflap_gcloud_cli.py
+++ sureflap_gcloud_cli.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
+import logging
+
 import sureflap_gcloud
 
 
 if __name__ == "__main__":
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
     sureflap_gcloud.set_curfew()
