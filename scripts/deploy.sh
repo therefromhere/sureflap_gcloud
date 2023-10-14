56b0c12024d56084cce69f955fcba7af0a513730
John Carter <john@therefromhere.org>
2023-10-14 21:35:47 +1300

    Enable deploy on pr (#6)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 ./scripts/generate_requirements.sh
-gcloud functions deploy update --runtime=python311 --trigger-http --env-vars-file=.env_vars.yaml --project=$GOOGLE_CLOUD_PROJECT
+gcloud functions deploy update --runtime=python311 \
+  --trigger-http --env-vars-file=.env_vars.yaml \
+  --set-secrets=ASTRAL_LOCATION=projects/$GOOGLE_CLOUD_PROJECT/secrets/ASTRAL_LOCATION:latest,SUREPY_AUTH_TOKEN=projects/$GOOGLE_CLOUD_PROJECT/secrets/SUREPY_AUTH_TOKEN:latest \
+  --project=$GOOGLE_CLOUD_PROJECT
