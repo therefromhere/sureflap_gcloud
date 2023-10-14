a497f81cbed67c12d0fb467922160ce07c7e15e4
John Carter <john@therefromhere.org>
2023-10-14 21:32:18 +1300

    Update deploy script to match github

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@
 ./scripts/generate_requirements.sh
 gcloud functions deploy update --runtime=python311 \
   --trigger-http --env-vars-file=.env_vars.yaml \
-  --set-secrets=projects/catflap/secrets/ASTRAL_LOCATION,SUREPY_AUTH_TOKEN=projects/catflap/secrets/SUREPY_AUTH_TOKEN:latest \
+  --set-secrets=ASTRAL_LOCATION=projects/$GOOGLE_CLOUD_PROJECT/secrets/ASTRAL_LOCATION:latest,SUREPY_AUTH_TOKEN=projects/$GOOGLE_CLOUD_PROJECT/secrets/SUREPY_AUTH_TOKEN:latest \
   --project=$GOOGLE_CLOUD_PROJECT
