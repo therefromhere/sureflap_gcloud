61c75a046eb82c5177726c943cba4b0b11b66915
John Carter <john@therefromhere.org>
2024-10-13 11:07:47 +1300

    Switch from poetry to uv (#80)
    Also switch cloud function to python 3.12

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./scripts/generate_requirements.sh
-gcloud functions deploy update2 --gen2 --runtime=python311 --region=us-central1 \
+gcloud functions deploy update2 --gen2 --runtime=python312 --region=us-central1 \
   --trigger-http --env-vars-file=.env_vars.yaml \
   --allow-unauthenticated \
   --set-secrets=ASTRAL_LOCATION=projects/$GOOGLE_CLOUD_PROJECT/secrets/ASTRAL_LOCATION:latest,SUREPY_AUTH_TOKEN=projects/$GOOGLE_CLOUD_PROJECT/secrets/SUREPY_AUTH_TOKEN:latest \
