88142b8ec424a85547aa4432f2fd29edda07c51d
John Carter <john@therefromhere.org>
2025-07-20 11:40:45 +1200

    Switch to python 3.13

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 source ./scripts/_common_init.sh
 
 ./scripts/generate_requirements.sh
-gcloud functions deploy update2 --gen2 --runtime=python312 --region=us-central1 \
+gcloud functions deploy update2 --gen2 --runtime=python313 --region=us-central1 \
   --trigger-http --env-vars-file=.env_vars.yaml \
   --allow-unauthenticated \
   --set-secrets=ASTRAL_LOCATION=projects/$GOOGLE_CLOUD_PROJECT/secrets/ASTRAL_LOCATION:latest,SUREPY_TOKEN=projects/$GOOGLE_CLOUD_PROJECT/secrets/SUREPY_AUTH_TOKEN:latest \
